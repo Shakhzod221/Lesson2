2fd0c1e666bc853b9af9e27d1002cc54f40b3c8f
Shakhzod221 <130886474+Shakhzod221@users.noreply.github.com>
2023-09-20 00:46:43 +0500

    Shokhz

---
+++ main.js
@@ -0,0 +1,334 @@
+// let age = 20;
+
+
+// function logAge(){
+//     if(age > 18){
+//         console.log('hello');
+//     }
+// }
+
+
+
+// add(4, 2);
+
+
+// function add(x, y){
+//     console.log(x + y);
+// }
+
+
+// function add(a, b){
+//     console.log(a - b);
+// }
+
+
+// function add(d, e){
+//     console.log(d * e);
+// }
+
+
+// function add(f, g){
+//     console.log(f / g);
+// }
+
+
+
+            // Function
+
+
+
+// findOdd(7)
+
+// function findOdd(num){
+//     if(num % 2 == 0){
+//         console.log('Juft son');
+//     }else{
+//         console.log('Juft son emas');
+//     }
+// }
+
+
+        // for
+
+
+// let arr = [1,2,3,4,5,6];
+
+// for(let i = 0; i < arr.length; i++){
+//     console.log(i);
+// }
+
+
+        // 1 - Misol
+
+
+// let k = 1;
+
+// let n = 5;
+
+// for(let i = 0; i < n; i++){
+//     console.log(k);
+// }
+
+
+
+// let k = 11;
+
+// let n = 5;
+
+// function loopNum(k, n){
+//     for(let i = 1; i < n; i++){
+//         console.log(k);
+//     }
+// }
+
+
+
+        // 2 - Misol
+
+
+// let a = 11;
+
+// let b = 7;
+
+// if(a > b){
+//     for(let i = a; i >= b; i--){
+//         console.log(i);
+//     }
+// }
+
+
+
+        // 3 - Misol
+
+
+// let a = 1;
+
+// let b = 10;
+
+// for(let i = 1; i <= b; i++){
+//     console.log(i * 10000);
+// }
+
+
+
+        // 4 - Misol
+
+
+// let a = 0.1;
+
+// let b = 1;
+
+// for(let i = a; i <= b; i++){
+//     console.log(i * 10000);
+// }
+
+
+
+// let konfet = 10000;
+
+// function konfetPrice(){
+//     for(let i = 1; i <= 10; i++){
+//         console.log(
+//             i * konfet
+//         );
+//     }
+// }
+
+
+
+        // 5 - Misol
+
+
+
+// let a = 0.1;
+
+// let b = 1;
+// function konfetPrice(){
+//     for(let i = a; i <= 1; i += 0.1){
+//         console.log(i * 10000);
+//     }
+// }
+
+
+
+        // 6 - Misol
+
+
+
+// let a = 1.2;
+
+// let b = 2;
+
+// function konfetPrice(){
+//     for(let i = a; i <= b; i += 0.2){
+//         console.log(i * 10000);
+//     }
+// }
+
+
+        // 7 - Misol
+
+
+
+// let a = 1;
+
+// let b = 10;
+
+// let count = 0;
+
+// function namAdd(){
+//     for(let i = a; i <= b; i++){
+//         count += i
+//     }
+//     console.log(count);
+// }
+
+
+
+        // 8 - Misol
+
+
+
+// let a = 1;
+
+// let b = 5;
+ 
+// let count = 1;
+
+// function numMult(){
+//         for(let i = a; i <= b; i++){
+//                 count *= i
+//         }
+//         console.log(count);
+// }
+
+
+
+        // 9 - Misol
+
+
+
+// let a = 1;
+
+// let b = 5;
+
+// let count = 0;
+
+// let kopaytma = 1;
+
+// function numAdd(){
+//         for(let i = a; i <= b; i++){
+//                 kopaytma = i * i
+//                 count += kopaytma
+//         }
+//         console.log(count);
+// }
+
+
+
+        // 11 - Misol
+
+
+
+// let n = 20;
+
+// function numOdd(){
+//         for(let i = 1; i <= n; i++){
+//                 if(!(i % 2 == 0)){
+//                         console.log(i);
+//                 }
+//         }
+// }
+
+
+
+        // 12 - Misol
+
+
+
+// let n = 15;
+
+// function numEdd(){
+//         for(let i = 1; i <= n; i++){
+//                 if(i % 2 == 0){
+//                         console.log(i);
+//                 }
+//         }
+// }
+
+
+
+        // 13 - Misol
+
+
+// let n = 12;
+
+// function numComp(){
+//         for(let i = 1; i <= n; i++){
+//                 if(n % i == 0){
+//                         console.log(i);
+//                 }
+//         }
+// }
+
+
+
+        // 14 - Misol
+
+
+
+// let n = 10;
+
+// function numAdd(){
+//         for(let i = 1; i <= n; i++){
+//                 console.log(i * i);
+//         }
+// }
+
+
+
+        // 15 -Misol
+
+
+// let n = 10;
+
+// function num(){
+//         for(let i = n; i >= 0; i--){
+//                 console.log(i);
+//         }
+// }
+
+
+
+        // 16 - Misol
+
+
+// let n = 50;
+
+// function numMul(){
+//         for(let i = 0; i <= n; i++){
+//                 if(i % 5 == 0){
+//                         console.log('Shokh');
+//                 }else{
+//                         console.log(i);
+//                 }
+//         }
+// }
+
+
+
+        // 17 - Misol
+
+
+// let a = 5;
+
+// let count = 1;
+                        
+// function numAdd(){
+//         for(let i = 1; i <= a; i++){
+//                 count *= i
+//         }
+//         console.log(
+//                 count
+//         );
+// }
